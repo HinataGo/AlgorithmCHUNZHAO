952663bf486ad0dd0cbefc99aec407d3979e3f6d
HinataGo <hinatashoyo@foxmail.com>
2021-01-20 21:44:11 +0800

    1.20 第一天学习 数组 链表 跳表 lru

---
+++ Week_01/mon/move_zero.go
@@ -0,0 +1,20 @@
+package mon
+
+func moveZeroes(nums []int) {
+	// 原地操作
+	// 遍历交换
+	// 双指针 , l 0  ,r !0  swap
+	// 一定要同时从0 开始 , 防止 连续两个不为0 的数交换,,乱次序
+	// 让l 先 r 一步,随后只有当 nums[r] == 0 ,两者才会错开, 否则两者同步
+	// 交换自然不会出错
+	l, r := 0, 0
+
+	for r < len(nums) {
+		if nums[r] != 0 {
+			nums[l], nums[r] = nums[r], nums[l]
+			l++
+		}
+		r++
+	}
+
+}
