184d495f62236e57b479e55839bee3478840a6fb
HinataGo <365264652@qq.com>
2021-02-01 10:31:31 +0800

    2021.2.1 上周三 验证二叉搜索树 添加注释

--- Week_02/wen/is_valid_BST.go
+++ Week_02/wen/is_valid_BST.go
@@ -4,7 +4,7 @@ import "math"
 
 // 方法1. BST 中序遍历数据递增
 // 方法2. 写一个递归
-// 综合一下,遍历过程中就进行对比
+// 综合一下,遍历过程中就进行对比, 递归大法好!
 
 var pre = math.MinInt64
 
@@ -15,16 +15,21 @@ func isValidBST(root *TreeNode) bool {
 }
 
 func is(root *TreeNode) bool {
+	// 基本条件检查
 	if root == nil {
 		return true
 	}
+	// 访问左子树
 	if !is(root.Left) {
 		return false
 	}
+	// 中序遍历的升序验证
 	if root.Val <= pre {
 		return false
 	}
+	//  更新前一个节点值
 	pre = root.Val
+	// 访问右子树
 	return is(root.Right)
 }
 
