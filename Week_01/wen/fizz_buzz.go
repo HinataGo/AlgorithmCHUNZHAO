5e4dc27391da24fa5afc21df4a0d1c8d8c2a9392
HinataGo <365264652@qq.com>
2021-02-03 12:49:56 +0800

    update  每日一题 412. Fizz Buzz 1.23

---
+++ Week_01/wen/fizz_buzz.go
@@ -0,0 +1,19 @@
+package wen
+
+import "strconv"
+
+func fizzBuzz(n int) []string {
+	res := make([]string, 0)
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 != 0 {
+			res = append(res, "Fizz")
+		} else if i%5 == 0 && i%3 != 0 {
+			res = append(res, "Buzz")
+		} else if i%5 == 0 && i%3 == 0 {
+			res = append(res, "FizzBuzz")
+		} else {
+			res = append(res, strconv.Itoa(i))
+		}
+	}
+	return res
+}
