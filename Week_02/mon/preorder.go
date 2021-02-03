8359529285b9c7eb42bf53314b083922a11e9189
HinataGo <365264652@qq.com>
2021-02-03 12:59:09 +0800

    fix

---
+++ Week_02/mon/preorder.go
@@ -0,0 +1,35 @@
+package mon
+
+// N叉树遍历 前序
+
+func preorder(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+	var res []int
+	res = append(res, root.Val)
+	for _, v := range root.Children {
+		res = append(res, preorder(v)...)
+	}
+	return res
+}
+
+// func preorder(root *Node) (res []int ){
+// 	var pre func(ndoe * Node)
+// 	pre = func(node *Node){
+// 		if node == nil{
+// 			return
+// 		}
+// 		res = append(res, node.Val)
+// 		for _,v := range node.Children{
+// 			pre(v)
+// 		}
+// 	}
+// 	pre(root)
+// 	return
+// }
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
